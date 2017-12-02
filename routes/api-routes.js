b5faa7475cf19e6be0a504ec64c11c8f527f7d63
bioko131 <migueljbarrera@hotmail.com>
2017-12-02 00:04:45 -0600

    Adding quote generator to /user. Minor issue pending

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,158 +6,158 @@ var body = require("body-parser");
 var UserId;
 var StoryId;
 
-module.exports = function (app) {
+module.exports = function(app) {
 
-	//find all users and return them
-	app.get("/api/allusers", function (req, res) {
+    //find all users and return them
+    app.get("/api/allusers", function(req, res) {
 
-		db.User.findAll({}).then(function(allUsers){
-			res.json(allUsers);
-		});
-	});
+        db.User.findAll({}).then(function(allUsers) {
+            res.json(allUsers);
+        });
+    });
 
-	//need a get route to retrieve all story data and include all posts related to the story
-	app.get("/api/story", function (req, res) {
-		console.log(req.body);
+    //need a get route to retrieve all story data and include all posts related to the story
+    app.get("/api/story", function(req, res) {
+        console.log(req.body);
 
-		db.Story.findAll({
-			include: [db.Post]
-		}).then(function (data) {
-			res.json(data);
-			res.render(data);
-		});
-	});
-	
-	//this creates a new user and password
-	app.post("/api/newuser", function (req, res) {
-		console.log(req.body);
+        db.Story.findAll({
+            include: [db.Post]
+        }).then(function(data) {
+            res.json(data);
+            res.render(data);
+        });
+    });
 
-		db.User.create({
-			userName: req.body.name,
-			password: req.body.password,
-			email: req.body.email
-		}).then(function(data){
-			res.send(data);
-			//console.log(data);
-			console.log("---------------");
-			//console.log(data.id);
+    //this creates a new user and password
+    app.post("/api/newuser", function(req, res) {
+        console.log(req.body);
 
-			UserId = data.id;
-		});
-	});
+        db.User.create({
+            userName: req.body.name,
+            password: req.body.password,
+            email: req.body.email
+        }).then(function(data) {
+            res.send(data);
+            //console.log(data);
+            console.log("---------------");
+            //console.log(data.id);
 
-	//this route creates a story thread using the story title data from story.js file
-	app.post("/api/story", function (req, res) {
-		console.log(req.body);
+            UserId = data.id;
+        });
+    });
 
-		//var UserId = req.session.id;
+    //this route creates a story thread using the story title data from story.js file
+    app.post("/api/story", function(req, res) {
+        console.log(req.body);
 
-		db.Story.create({
-			storyName: req.body.storyName,
-			UserId: UserId
-			//UserId
-			//req.body.UserId
-			//UserId: NEED TO GRAB USER ID FROM TABLE AND SEND WITH STORY CREATION REQUEST
-		}).done(function(data){
-			
-			//returns the created story as a json object
-			res.json(data);
-			console.log("1-1-1-1-1-1-1-1-1-1-1");
-			console.log(data.id);
+        //var UserId = req.session.id;
 
-			StoryId = data.id;
+        db.Story.create({
+            storyName: req.body.storyName,
+            UserId: UserId
+                //UserId
+                //req.body.UserId
+                //UserId: NEED TO GRAB USER ID FROM TABLE AND SEND WITH STORY CREATION REQUEST
+        }).done(function(data) {
 
-			//render the created story to the page using res.render("index", data)
+            //returns the created story as a json object
+            res.json(data);
+            console.log("1-1-1-1-1-1-1-1-1-1-1");
+            console.log(data.id);
 
-		});
-		
-	});
+            StoryId = data.id;
 
-	app.post("/api/post", function (req, res) {
+            //render the created story to the page using res.render("index", data)
 
-		console.log(req.body);
+        });
 
-		//var UserId = req.session.id;
+    });
 
-		//creates a post that includes the post text (body) and the related story ID
-		db.Post.create({
-			body: req.body.body,
-			StoryId: StoryId,
-			UserId: UserId
-			//UserId: NEEDS TO BE GRABBED FROM USER TABLE
-		}).done(function(data){
-			res.json(data);
-		});
-	});
+    app.post("/api/post", function(req, res) {
 
-	//login attempt checks to see if account with same password exists in user table
-	app.post("/api/login", function (req, res) {
+        console.log(req.body);
 
-		console.log("-2-2-2-2-2-2-2-2");
-		console.log(req.body);
+        //var UserId = req.session.id;
 
-		db.User.findOne({
-			where: {
-				email: req.body.email,
-				password: req.body.password
-			}
-		}).done(function (data) {
-			
-			res.json(data);
-			//if there was no data returned (aka incorrect info recevied from frontend breaking the request)
-			//then log OOPSS!! 
-			if (!data) {
-				console.log("OOPPPSS!!!");
-			};
+        //creates a post that includes the post text (body) and the related story ID
+        db.Post.create({
+            body: req.body.body,
+            StoryId: StoryId,
+            UserId: UserId
+                //UserId: NEEDS TO BE GRABBED FROM USER TABLE
+        }).done(function(data) {
+            res.json(data);
+        });
+    });
 
-			//Otherwise tell me the name of the user you found
-			console.log("Found User " + data.userName);
-			
-			
-			UserId = data.id;
-		});
-	});
+    //login attempt checks to see if account with same password exists in user table
+    app.post("/api/login", function(req, res) {
 
-	app.get("/api/user", function (req, res) {
-		
-		console.log(req.body);
+        console.log("-2-2-2-2-2-2-2-2");
+        console.log(req.body);
 
-		db.Story.findAll({}).then(function (data) {
-			
-			var context = {
-				userStoriesArray: [data],
-				storyName: data.storyName
-			};
+        db.User.findOne({
+            where: {
+                email: req.body.email,
+                password: req.body.password
+            }
+        }).done(function(data) {
 
-			res.render("loggedInUserView", context);
-			console.log("-----------------");
-			console.log(data);
-			console.log("------------------");
-			//render results to the page using res.render("index", data)
-		});
+            res.json(data);
+            //if there was no data returned (aka incorrect info recevied from frontend breaking the request)
+            //then log OOPSS!! 
+            if (!data) {
+                console.log("OOPPPSS!!!");
+            };
 
-		// db.User.findOne({
-		// 	where: {
-		// 		userName: req.params.name,
-		// 		password: req.body.password
-		// 	}
-		// }).done(function (data) {
-			
-		// 	res.json(data);
-		// 	//if there was no data returned (aka incorrect info recevied from frontend breaking the request)
-		// 	//then log OOPSS!! 
-		// 	if (!data) {
-		// 		console.log("OOPPPSS!!!");
-		// 	};
+            //Otherwise tell me the name of the user you found
+            console.log("Found User " + data.userName);
 
-		// 	//Otherwise tell me the name of the user you found
-		// 	console.log("Found User " + data.userName);
-		// 	console.log("1-1-1-1-1-1-1-1")
-		// 	//req.session.id = data.userName;
-		// 	console.log(req.session.id);
 
-		// });
-	});
+            UserId = data.id;
+        });
+    });
+
+    app.get("/api/user", function(req, res) {
+
+        console.log(req.body);
+
+        db.Story.findAll({}).then(function(data) {
+
+            var context = {
+                userStoriesArray: [data],
+                storyName: data.storyName
+            };
+
+            res.render("loggedInUserView", context);
+            console.log("-----------------");
+            console.log(data);
+            console.log("------------------");
+            //render results to the page using res.render("index", data)
+        });
+
+        // db.User.findOne({
+        // 	where: {
+        // 		userName: req.params.name,
+        // 		password: req.body.password
+        // 	}
+        // }).done(function (data) {
+
+        // 	res.json(data);
+        // 	//if there was no data returned (aka incorrect info recevied from frontend breaking the request)
+        // 	//then log OOPSS!! 
+        // 	if (!data) {
+        // 		console.log("OOPPPSS!!!");
+        // 	};
+
+        // 	//Otherwise tell me the name of the user you found
+        // 	console.log("Found User " + data.userName);
+        // 	console.log("1-1-1-1-1-1-1-1")
+        // 	//req.session.id = data.userName;
+        // 	console.log(req.session.id);
+
+        // });
+    });
 
 
 }
